4c9065f53794a7d8a6fef15a530c10d5e8410567
Miguel Arguelles <cmarguel@gmail.com>
2023-11-06 02:40:07 +0800

    first pass v1

--- main.js
+++ main.js
@@ -1,45 +1,88 @@
-YAML.load('timeline.yml', function(result)
-{
-    timeline = result;
-});
+let timeline = YAML.load('timeline.yml'); 
+let chapters = {};
+
+yamlString = YAML.stringify(timeline);
+
+// addSpacers(0);
+let prev = null;
 for(i = 0; i < timeline.length; i++) {
+    let info = timeline[i];
+    chapters[info.title] = info;
+    
+    let node = createNode(info);
+
+    document.getElementById("rank" + info.rank).appendChild(node);
+    addSpacers(info.rank);
+
+    if(info.prereqs) {
+        drawArrows(info, prev);
+    }
+    if(info.type == "jump") {
+        addSpacer(info.rank);
+        let spacer = addSpacer(info.rank);
+        new LeaderLine(
+            document.getElementById("c" + info.id),
+            spacer,
+            {dash: true}
+        );
+    }
+    prev = info.title;
+}
+
+function drawArrows(info, prev) {
+    // For convenience, I made it so that having a prereq of "p" makes 
+    // the immediate preceding node into the parent. 
+    // You can still manually specify a title, and you'll have to if
+    // there's multiple parents.
+    if (info.prereqs == "p") { 
+        let start = "c" + chapters[prev].id;
+        let end = "c" + info.id;
+
+        new LeaderLine(
+            document.getElementById(start),
+            document.getElementById(end),
+        );
+    } else {
+        for (j = 0; j < info.prereqs.length; j++) {
+            let start = "c" + chapters[info.prereqs[j]].id;
+            let end = "c" + info.id;
+
+            new LeaderLine(
+                document.getElementById(start),
+                document.getElementById(end),
+            );
+        }
+    }
+}
+
+function createNode(info) {
     var node = document.createElement("div");
+    node.id = "c" + info.id;
     node.className = "node";
-    var heading = document.createTextNode(timeline[i].title);
-    node.append(heading);
-    document.getElementById("div1").appendChild(node);
+
+    var link = document.createElement("a");
+    link.href = info.url;
+    link.title = info.title;
+
+    var heading = document.createTextNode(info.title);
+    link.append(heading);    
+    node.append(link);
+
+    return node;
 }
 
-
-var cols = document.getElementsByClassName('column');
-for(i = 0; i < cols.length; i++) {
-    cols[i].style.width = 15 + "%";
+function addSpacers(rank) {
+    for (r = 1; r <= 5; r++) {
+        if (r != rank) {
+            addSpacer(r);
+        }
+    }
 }
 
+function addSpacer(r) {
+    let spacer = document.createElement("div");
+    spacer.className = "spacer node";
+    document.getElementById("rank" + r).appendChild(spacer);
+    return spacer;
+}
 
-// Add new leader line from `start` to `end` (HTML/SVG element, basically).
-new LeaderLine(
-    document.getElementById('start'),
-    document.getElementById('mid1'),
-    // { path: "grid" }
-  );
-  new LeaderLine(
-    document.getElementById('mid1'),
-    document.getElementById('end'),
-    // { path: "grid" }
-  );
-  new LeaderLine(
-    document.getElementById('mid2'),
-    document.getElementById('end'),
-    // { path: "grid" }
-  );
-  new LeaderLine(
-    document.getElementById('mid1'),
-    document.getElementById('mid2'),
-    // { path: "grid" }
-  );
-  new LeaderLine(
-    document.getElementById('mid3'),
-    document.getElementById('end'),
-    // { path: "grid" }
-  );
