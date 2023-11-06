13751cb52f2e6949d0833b1b0757b934a97e33d0
Miguel Arguelles <cmarguel@gmail.com>
2023-11-06 17:59:22 +0800

    more updates

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 let timeline = YAML.load('timeline.yml'); 
 let chapters = {};
+let existingSprites = ["erin", "ryoka", "horns"];
 
 yamlString = YAML.stringify(timeline);
 
 // addSpacers(0);
 let prev = null;
-for(i = 0; i < timeline.length; i++) {
+for(let i = 0; i < timeline.length; i++) {
     let info = timeline[i];
     chapters[info.title] = info;
     
@@ -43,7 +44,7 @@ function drawArrows(info, prev) {
             document.getElementById(end),
         );
     } else {
-        for (j = 0; j < info.prereqs.length; j++) {
+        for (let j = 0; j < info.prereqs.length; j++) {
             let start = "c" + chapters[info.prereqs[j]].id;
             let end = "c" + info.id;
 
@@ -59,6 +60,15 @@ function createNode(info) {
     var node = document.createElement("div");
     node.id = "c" + info.id;
     node.className = "node";
+    if (info.catchup) {
+        node.className += " dataNode";
+
+        var dataPanel = document.createElement("div");
+        dataPanel.className = "dataPanel";
+        var catchupText = document.createTextNode(info.catchup);
+        dataPanel.append(catchupText);
+        node.append(dataPanel);
+    }
 
     var link = document.createElement("a");
     link.href = info.url;
@@ -68,11 +78,53 @@ function createNode(info) {
     link.append(heading);    
     node.append(link);
 
+    addSprites(node, info);
+
     return node;
 }
 
+function addSprites(node, info) {
+    if(info.povs) {
+        let charDiv = document.createElement("div");
+        charDiv.className = "povList";
+    
+        for(let i = 0; i < info.povs.length; i++) {
+            let char = info.povs[i];
+            if (!existingSprites.includes(char)) {
+                char = "unknown";
+            }
+
+            let span = document.createElement("span");
+            span.className = "povSprite";
+            // span.src = "sprites/" + char + "-sprite.png";
+            span.style = "height: 16px; width: 16px; background-image: url(sprites/" + char + "-sprite.png);";
+            charDiv.append(span);
+        }
+        node.append(charDiv);
+    }
+
+    if(info.guests) {
+        let charDiv = document.createElement("div");
+        charDiv.className = "guestList";
+    
+        for(let i = 0; i < info.povs.length; i++) {
+            let char = info.guests[i];
+            if (!existingSprites.includes(char)) {
+                char = "unknown";
+            }
+
+            let span = document.createElement("span");
+            span.className = "guestSprite";
+            // span.src = "sprites/" + char + "-sprite.png";
+            span.style = "height: 16px; width: 16px; background-image: url(sprites/" + char + "-sprite.png);";
+            charDiv.append(span);
+        }
+        node.append(charDiv);
+    }
+}
+
 function addSpacers(rank) {
-    for (r = 1; r <= 5; r++) {
+    for (let r = 1; r <= 5; r++) {
         if (r != rank) {
             addSpacer(r);
         }
